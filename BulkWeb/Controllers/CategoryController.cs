00ed48f40aeddbb0edb451363dc632ab183e2907
NikolaiChelovekich <nikolay.gonchar.2002@mail.ru>
2023-08-16 22:01:18 +0300

    day4 learning mvc: add edit,delete and notifications

--- BulkWeb/Controllers/CategoryController.cs
+++ BulkWeb/Controllers/CategoryController.cs
@@ -28,15 +28,81 @@ namespace BulkWeb.Controllers
             {
                 ModelState.AddModelError("name", "Display order cannot exactly match the Name");
             }
-            if(ModelState.IsValid)
+
+            if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["succes"] = "Category created succesfully!";
                 return RedirectToAction("Index","Category"); 
             }
 
             return View(obj);
 
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category categoryFromDb = _db.Categories.Find(id);
+
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["succes"] = "Category updated succesfully!";
+                return RedirectToAction("Index", "Category");
+            }
+
+            return View(obj);
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            _db.Remove(categoryFromDb);
+            _db.SaveChanges();
+            TempData["succes"] = "Category deleted succesfully!";
+            return RedirectToAction("Index", "Category");
         }
     }
 }
